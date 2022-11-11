1810c9140f5ec71963f555f995443cc8c87742e9
mattv52 <matthewvolaris@yahoo.com.au>
2022-11-11 11:03:38 +0000

    added ability to make blocks turn invisible and toggle with G key

---
+++ My project/Assets/Scripts/BlockManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    private bool blocks = true; 
+    public SpriteRenderer[] spriteRenderes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderes = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire3"))
+        {
+            toggleBlocks();
+        }
+    }
+
+    // Turn blocks on/off
+    void toggleBlocks()
+    {
+        blocks = !blocks;
+        for (int i = 0; i < spriteRenderes.Length; i++)
+        {
+            spriteRenderes[i].enabled = blocks;
+        }
+    }
+}
